dfb2557dd38c6a31314dab3ef84e3c6de78e7dc5
mak09509 <frozencat1216@gmail.com>
2025-09-01 12:24:03 +0700

    9/1/2025

---
+++ TOI/orb.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, L, a, b, temp;
+set<int> arr;
+
+int main(void) {
+
+    cin >> N >> L >> a >> b;
+    for (int i = 0; i <N; i++) {
+        cin >> temp;
+        arr.emplace(temp);
+    }
+
+    for (int i = 0; i < L; i++) {
+
+    }
+    
+    return 0;
+}
